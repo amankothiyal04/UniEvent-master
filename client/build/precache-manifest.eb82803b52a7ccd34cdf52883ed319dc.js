66dc224ba045f9f9b4625a8f260a44319523afd7
shahrukh khan <shahrukhbadsah@gmail.com>
2021-11-13 22:46:56 +0530

    events

---
+++ client/build/precache-manifest.eb82803b52a7ccd34cdf52883ed319dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa54eb737edecf1bec33412f4ab76f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40224cf09918cf1e976",
+    "url": "/static/css/main.f8947faa.chunk.css"
+  },
+  {
+    "revision": "e334eec7e0ba12150376",
+    "url": "/static/js/2.210e8925.chunk.js"
+  },
+  {
+    "revision": "c40224cf09918cf1e976",
+    "url": "/static/js/main.648e9ed1.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "d6d1923549e9233deb6dfebb95bf5d76",
+    "url": "/static/media/event.d6d19235.jpeg"
+  }
+]);
